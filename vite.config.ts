3c64eba44fdef40e32c064c4f671b287c3856630
giriraj-parsewar <girirajparsewar2003@gmail.com>
2025-04-14 10:45:54 +0530

    configuring files for github page view

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nutriscanner-app/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
